df8a0221443b747829d43a647d722bb1d1be4b4a
CamileGhastine <camileghastine@hotmail.com>
2022-12-16 13:07:46 +0100

    admin: admin area, delete post, publish post

--- src/Controller/AdminController.php
+++ src/Controller/AdminController.php
@@ -3,9 +3,11 @@
 namespace App\Controller;
 
 use App\Entity\Post;
+use App\Repository\PostRepository;
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
 use Symfony\Bundle\SecurityBundle\Security;
 use Symfony\Component\Finder\Exception\AccessDeniedException;
+use Symfony\Component\HttpFoundation\Request;
 use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\Routing\Annotation\Route;
 use Symfony\Component\Security\Http\Attribute\IsGranted;
@@ -13,6 +15,10 @@ use Symfony\Component\Security\Http\Attribute\IsGranted;
 // #[isGranted('ROLE_ADMIN')]
 class AdminController extends AbstractController
 {
+    public function __construct(private PostRepository $postRepository)
+    {
+    }
+
     #[Route('/admin', name: 'admin')]
     // #[IsGranted('ROLE_ADMIN', statusCode: 403, message: 'Accès refuser aux nom-admins')]
     public function index(): Response
@@ -31,6 +37,40 @@ class AdminController extends AbstractController
         //     return $this->redirectToRoute('home');
         // }
 
-        return $this->render('admin/index.html.twig', );
+
+
+        return $this->render('admin/index.html.twig', [
+            'posts' => $this->postRepository->findAll()
+        ]);
+    }
+
+    #[Route('/admin/post/delete/{id<[0-9]+>}', name: 'delete_post')]
+    #[isGranted('ROLE_ADMIN')]
+    public function delete(Post $post, Request $request): Response
+    {
+        $token = $request->query->get('token');
+
+        if($this->isCsrfTokenValid('deletePost' . $post->getId(), $token)) {
+            $this->addFlash('success', 'L\'article a été supprimé avec succès.');
+
+            $this->postRepository->remove($post, true);
+        }
+
+        return $this->redirectToRoute('admin');
+    }
+
+    #[Route('/admin/post/publis/{id<[0-9]+>}', name: 'publish_post')]
+    #[isGranted('ROLE_ADMIN')]
+    public function publish(Post $post, Request $request): Response
+    {
+        $token = $request->query->get('token');
+
+        if($this->isCsrfTokenValid('publish' . $post->getId(), $token)) {
+            $post->setIsPublished(!$post->isIsPublished());
+
+            $this->postRepository->save($post, true);
+        }
+
+        return $this->redirectToRoute('admin');
     }
 }
